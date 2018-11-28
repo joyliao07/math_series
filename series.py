893b93b6f57f541fe44a37283e25ab70eecb9ff1
Joyce Liao <joyliao07@gmail.com>
2018-11-27 17:48:36 -0800

    pass user input to eval()

--- series.py
+++ series.py
@@ -1,10 +1,12 @@
-fib = [0, 1]
+import sys
+status = True
+
 # THIS IS THE FABONACCI SERIES:
 # 0, 1, 1, 2, 3, 5, 8, ETC.
 
 
-
 def fibonacci(n):
+    fib = [0, 1]
     #DOC: A FUNCTION THAT RETURNS THE nth VALUE OF THE FABONACCI SERIES
     if n > 2:
         for what in range(3, n+1):
@@ -17,9 +19,7 @@ def fibonacci(n):
 
 def lucas(n):
     #DOC: A FUNCTION THAT RETURNS THE nth VALUE OF THE LUCAS NUMBERS
-    pass
-
-
+    print('we did lucas')
 
 
 def sum_series():
@@ -31,10 +31,37 @@ def test():
     return True
 
 
+def question():
+    print('Please enter your function and input.')
+    user_input = input()
+    if user_input == 'quit':
+        exit()
+    else:
+        eval(user_input)
+
 
 if __name__ == "__main__":
-    # fibonacci(6)
-    pass
+    #THIS IS TO SUMMARIZE WHAT THE PROGRAM DOES
+    print('''
+    This module defines functions that implement mathematical series.
+
+    fibonacci(n):
+        Returns the nth value in the fabonacci series.
+
+    lucas(n):
+        Returns the nth value in the lucas numbers.
+
+    sum_series(n):
+        Returns the nth value in the sum series, with the beginning two numbers in the number series an option for your input.
+
+    To quit the program at any time, enter "quit"
+    ''')
+    user_input = '' #THIS VARIABLE ASSIGNMENT IS REQUIRED
+
+    while status is True:
+        question()
+
+
 
 
 
