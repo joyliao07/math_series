c30a95a06b3fdf2fc911c837f27e1422dbf72632
Joyce Liao <joyliao07@gmail.com>
2018-12-01 12:07:16 -0800

    raise exception with invalid input

--- test_series.py
+++ test_series.py
@@ -13,7 +13,7 @@ def test_fibonacci_too_big():
     """TO TEST FIBONACCI WITH AN INVALID INPUT THAT IS TOO BIG."""
     from .series import fibonacci
     input = 1000
-    expected = 'The number is too big.'
+    expected = 'The number is too big. Please enter a number between 0 and 999.'
     assert fibonacci(input) == expected
 
 
@@ -37,7 +37,7 @@ def test_lucas_too_big():
     """TO TEST LUCAS WITH AN INVALUD INPUT THAT IS TOO BIG"""
     from .series import lucas
     input = 1000
-    expected = 'The number is too big.'
+    expected = 'The number is too big. Please enter a number between 0 and 999.'
     assert lucas(input) == expected
 
 
@@ -61,7 +61,7 @@ def test_sum_series_too_big():
     """TO TEST SUM_SERIES WITH AN INVALID INPUT THAT'S TOO BIG."""
     from .series import sum_series
     input = 1000
-    expected = 'The number is too big.'
+    expected = 'The number is too big. Please enter a number between 0 and 999.'
     assert sum_series(input) == expected
 
 
@@ -71,3 +71,21 @@ def test_sum_series_negative():
     input = -1
     expected = 'The number cannot be negative.'
     assert sum_series(input) == expected
+
+
+def test_result_with_valid_input(capsys):
+    """To test result() to properly answer with a valid input."""
+    from .series import result
+    user_input = 'fibonacci(5)'
+    result(user_input)
+    captured = capsys.readouterr()
+    assert captured.out == '5\n'
+
+
+def test_result_exception(capsys):
+    """To test result() to properly raise exceptions."""
+    from .series import result
+    user_input = 'I need a number.'
+    result(user_input)
+    captured = capsys.readouterr()
+    assert captured.out == 'Please enter a valid input. For example, "fibonacci(n)", "lucas(n)", or "sum_series(n)".\n'
